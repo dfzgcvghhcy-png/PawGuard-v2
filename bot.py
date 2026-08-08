af3f3aace38f7d54b5333f1b3d135aee44402122
EvanEloff <dfzgcvghhcy@gmail.com>
2026-08-08 17:50:49 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
 import os
 
 async def main():
     bot = Bot(token=os.getenv("BOT_TOKEN"))
     dp = Dispatcher()
+
+    @dp.message(Command("start"))
+    async def cmd_start(message: types.Message):
+        await message.answer("🐾 PawGuard работает!")
 
     print("🐾 PawGuard стартанул")
 
